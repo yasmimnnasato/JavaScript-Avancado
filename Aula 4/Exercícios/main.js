b30f527a2386794c49e3b621030ea527ba9c9832
yasmimnnasato <yasmimnasato.s@gmail.com>
2025-02-27 15:25:17 -0300

    atividade

--- Aula 4/Exercícios/main.js
+++ Aula 4/Exercícios/main.js
@@ -6,5 +6,18 @@ texto.innerText = "Hello Wolrd"
 let lista = document.querySelector(".lista")
 lista.style.backgroundColor = "pink"
 
-//Atividadee 3
-let input
+//Atividade 3
+let input =  document.querySelector(".value")
+input.value = "Digite algo aqui!"
+
+//Atividade 4
+let quadrado = document.querySelector(".quadrado")
+quadrado.style.width = "40px"
+quadrado.style.height= "40px"
+quadrado.style.backgroundColor = "purple"
+
+//Desafio 1
+
+//Atividade 1.2
+let botao = document.querySelector (".botao")
+botao.addEventListener (dbclick) 
